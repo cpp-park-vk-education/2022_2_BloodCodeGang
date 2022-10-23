2ec5e30512ff080a7db7bbbf07e2222a765d1425
Vanvik <ivanspecial5@gmail.com>
2022-10-23 19:54:51 +0000

    No memory error

--- memoryError/memoryError.cpp
+++ memoryError/memoryError.cpp
@@ -1,13 +1,13 @@
 #include "memoryError.h"
 
 
-int * memoryLost(int arrSize = 5) {  // Выделяем память, но не удаляем ее
-    int *array = new int[arrSize];
-    return array;
-}
+// int * memoryLost(int arrSize = 5) {  // Выделяем память, но не удаляем ее
+//     int *array = new int[arrSize];
+//     return array;
+// }
 
-void extraIndex(int arrSize = 5) {  // Обращаемся к несуществующему элементу массива (выход за рамки)
-    int *array = new int[arrSize];
-    array[arrSize] = 1;
-    array[arrSize+1] = 2;
-}
+// void extraIndex(int arrSize = 5) {  // Обращаемся к несуществующему элементу массива (выход за рамки)
+//     int *array = new int[arrSize];
+//     array[arrSize] = 1;
+//     array[arrSize+1] = 2;
+// }
