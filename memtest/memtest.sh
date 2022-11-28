033deaa803c9f8ba7b921a8cc9d7d2171b880ae0
Alexey <ax.chinaev@yandex.ru>
2022-11-28 20:03:32 +0000

    fix cov and mem

--- memtest/memtest.sh
+++ memtest/memtest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH="./bit"
+PROJECT_PATH="./build/bit"
 VALGRIND_LOG="valgrind.log"
 NO_LOST_PATTERN="All heap blocks were freed -- no leaks are possible"
 NO_ERROR_PATTERN="ERROR SUMMARY: 0 errors"
