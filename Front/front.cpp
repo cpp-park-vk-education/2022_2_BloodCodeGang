9fc349af441e005b590480e03109b0ffaf4a0b75
Alexey <ax.chinaev@yandex.ru>
2022-12-13 09:03:37 +0300

    made proper skeleton and compile it with simple text

---
+++ Front/front.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Алексей on 12.12.2022.
+//
+
+
+#include <Wt/WApplication.h>
+#include <Wt/WServer.h>
+
+#include "include/MenuWidget.h"
+//#include "Session.h"
+
+
+std::unique_ptr<Wt::WApplication> createApplication(const Wt::WEnvironment& env) {
+    auto app = std::make_unique<Wt::WApplication>(env);
+
+    app->setTitle("Friendify");
+
+//    app->messageResourceBundle().use(app->appRoot() + "strings");
+//    app->messageResourceBundle().use(app->appRoot() + "templates");
+
+//    app->useStyleSheet("css/hangman.css");
+
+    app->root()->addWidget(std::make_unique<MenuWidget>());
+
+    return app;
+}
+
+
+int main(int argc, char **argv)
+{
+    try {
+        Wt::WServer server(argc, argv, WTHTTP_CONFIGURATION);
+
+        server.addEntryPoint(Wt::EntryPointType::Application, createApplication);
+
+//        Session::configureAuth();
+
+        server.run();
+    } catch (Wt::WServer::Exception& e) {
+        std::cerr << e.what() << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "exception: " << e.what() << std::endl;
+    }
+}
--- cmake-build-debug/front/cmake_install.cmake
+++ cmake-build-debug/front/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/aleksej/Projects/2022_2_BloodCodeGang
+# Install script for directory: /Users/aleksej/Projects/2022_2_BloodCodeGang/front
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,19 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("/Users/aleksej/Projects/2022_2_BloodCodeGang/cmake-build-debug/front/cmake_install.cmake")
-
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/aleksej/Projects/2022_2_BloodCodeGang/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
