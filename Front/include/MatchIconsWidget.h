0866cc25d4a78202e8e5e117030127cf0d0c2d5b
Alexey <ax.chinaev@yandex.ru>
2022-12-15 17:32:26 +0300

    with css

---
+++ Front/include/MatchIconsWidget.h
@@ -0,0 +1,16 @@
+//
+// Created by Алексей on 13.12.2022.
+//
+
+#ifndef MYPROJECT_MATCHICONSWIDGET_H
+#define MYPROJECT_MATCHICONSWIDGET_H
+
+
+
+class MatchIconsWidget {
+
+};
+
+
+
+#endif //MYPROJECT_MATCHICONSWIDGET_H
--- Front/src/SwipeUIModel.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Алексей on 21.11.2022.
-//
-
-#include "../include/SwipeUIModel.h"
-
-
-void SwipeUIModel::getNextUser(bool like_value) {}
